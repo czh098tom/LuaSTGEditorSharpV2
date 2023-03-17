8868e4377537ed0588da2ffe1f799c81a7d72272
czh098tom <czh098tom@163.com>
2023-03-17 21:52:32 +0800

    Update CodeSelection

--- src/LuaSTGEditorSharpV2.Core.CodeGenerator/Configurable/CodeSelection.cs
+++ src/LuaSTGEditorSharpV2.Core.CodeGenerator/Configurable/CodeSelection.cs
@@ -9,12 +9,21 @@ using LuaSTGEditorSharpV2.Core.Model;
 namespace LuaSTGEditorSharpV2.Core.CodeGenerator.Configurable
 {
     [Serializable]
-    public record class CodeSelection(string Code, string ConditionOn, bool Inversed)
+    public record class CodeSelection(string Code, string ConditionOn, bool Inversed
+        , bool NullDefault = true, bool UseExistance = false)
     {
         public bool ShouldAppend(NodeData source)
         {
-            bool isTrue = source.GetProperty(ConditionOn, "true").ToLower().Trim() == "true";
-            return (isTrue && !Inversed) || (!isTrue && Inversed);
+            if (!UseExistance)
+            {
+                bool isTrue = source.GetProperty(ConditionOn, NullDefault.ToString()).ToLower().Trim() == "true";
+                return (isTrue && !Inversed) || (!isTrue && Inversed);
+            }
+            else
+            {
+                bool isTrue = string.IsNullOrWhiteSpace(source.GetProperty(ConditionOn, ""));
+                return (isTrue && !Inversed) || (!isTrue && Inversed);
+            }
         }
     }
 }
