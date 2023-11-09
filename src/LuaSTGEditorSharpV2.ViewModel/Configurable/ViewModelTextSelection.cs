90aca670f0e6d07ad06dbdd8b6f0962603facbaa
czh098tom <czh098tom@163.com>
2023-11-09 22:46:28 +0800

    review

--- src/LuaSTGEditorSharpV2.ViewModel/Configurable/ViewModelTextSelection.cs
+++ src/LuaSTGEditorSharpV2.ViewModel/Configurable/ViewModelTextSelection.cs
@@ -7,17 +7,26 @@ using LuaSTGEditorSharpV2.Core.Model;
 
 namespace LuaSTGEditorSharpV2.ViewModel.Configurable
 {
-	[Serializable]
-	public record class ViewModelTextSelection(string Text, string ConditionOn, bool Inversed)
-	{
-		public bool ShouldAppend(NodeData source)
-		{
-			string property = source.GetProperty(ConditionOn, "false");
-			bool isFalse = (
-				(property.ToLower().Trim() == "false")||
-				(String.IsNullOrEmpty(property))
-			);
-			return (!isFalse && !Inversed) || (isFalse && Inversed);
-		}
-	}
+    [Serializable]
+    public record class ViewModelTextSelection(string Text, string? ConditionOn,
+        bool UseExistanceInsteadOfBoolValue, bool Inversed)
+    {
+        public bool ShouldAppend(NodeData source)
+        {
+            if (ConditionOn == null)
+            {
+                return !Inversed;
+            }
+            bool isTrue;
+            if (!UseExistanceInsteadOfBoolValue)
+            {
+                isTrue = source.GetProperty(ConditionOn, "true").ToLower().Trim() == "true";
+            }
+            else
+            {
+                isTrue = string.IsNullOrWhiteSpace(source.GetProperty(ConditionOn, "true"));
+            }
+            return isTrue ^ Inversed;
+        }
+    }
 }
