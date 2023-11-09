c839e13f588b9c71ef3758b40ab0943d93249de4
czh098tom <czh098tom@163.com>
2023-11-09 21:58:47 +0800

    fixed bugs

--- src/LuaSTGEditorSharpV2.ViewModel/Configurable/SelectiveViewModelProvider.cs
+++ src/LuaSTGEditorSharpV2.ViewModel/Configurable/SelectiveViewModelProvider.cs
@@ -12,45 +12,41 @@ using LuaSTGEditorSharpV2.Core.Model;
 namespace LuaSTGEditorSharpV2.ViewModel.Configurable
 {
     [Serializable]
-    public class SelectiveViewModelProvider
+    public class SelectiveViewModelProvider : ViewModelProviderServiceBase
     {
-        [Serializable]
-        public class ConfigurableViewModelProvider : ViewModelProviderServiceBase
+        [JsonProperty] public string[] Captures { get; private set; } = Array.Empty<string>();
+        [JsonProperty] public string Icon { get; private set; } = "";
+        [JsonProperty] public ViewModelTextSelection[] Text { get; private set; } = Array.Empty<ViewModelTextSelection>();
+        [JsonProperty] public Dictionary<string, ViewModelTextSelection[]> LocalizedText { get; private set; } = new();
+
+        private string?[]? _captureResult;
+
+        protected override void UpdateViewModelData(NodeViewModel viewModel, NodeData dataSource, NodeViewModelContext context)
         {
-            [JsonProperty] public string[] Captures { get; private set; } = Array.Empty<string>();
-            [JsonProperty] public string Icon { get; private set; } = "";
-            [JsonProperty] public ViewModelTextSelection[] Text { get; private set; } = Array.Empty<ViewModelTextSelection>();
-            [JsonProperty] public Dictionary<string, ViewModelTextSelection[]> LocalizedText { get; private set; } = new();
-
-            private string?[]? _captureResult;
-
-            protected override void UpdateViewModelData(NodeViewModel viewModel, NodeData dataSource, NodeViewModelContext context)
+            _captureResult ??= new string[GetCaptureCacheLength()];
+            int n;
+            for (n = 0; n < Captures.Length; n++)
             {
-                _captureResult ??= new string[GetCaptureCacheLength()];
-                int n;
-                for (n = 0; n < Captures.Length; n++)
-                {
-                    _captureResult[n] = dataSource.GetProperty(Captures[n]);
-                }
-                ViewModelTextSelection[] text = LocalizedText
-                    .GetValueOrDefault(CultureInfo.CurrentUICulture.TwoLetterISOLanguageName, Text);
-                StringBuilder sb = new();
-                for (int i = 0; i < text.Length; i++)
-                {
-                    if (text[i].ShouldAppend(dataSource))
-                    {
-                        sb.AppendFormat(text[i].Text, _captureResult);
-                    }
-                }
-                viewModel.Text = sb.ToString();
-                viewModel.Icon = Icon;
+                _captureResult[n] = dataSource.GetProperty(Captures[n]);
             }
-
-            private int GetCaptureCacheLength()
+            ViewModelTextSelection[] text = LocalizedText
+                .GetValueOrDefault(CultureInfo.CurrentUICulture.TwoLetterISOLanguageName, Text);
+            StringBuilder sb = new();
+            for (int i = 0; i < text.Length; i++)
             {
-                int l = Captures.Length;
-                return l;
+                if (text[i].ShouldAppend(dataSource))
+                {
+                    sb.AppendFormat(text[i].Text, _captureResult);
+                }
             }
+            viewModel.Text = sb.ToString();
+            viewModel.Icon = Icon;
+        }
+
+        private int GetCaptureCacheLength()
+        {
+            int l = Captures.Length;
+            return l;
         }
     }
 }
