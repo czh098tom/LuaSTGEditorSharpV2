bd2cd5577938c3e136ac534f0ec56c50abc120c9
czh098tom <czh098tom@163.com>
2023-12-17 00:31:43 +0800

    vm use Localized<T>

---
+++ src/LuaSTGEditorSharpV2.ViewModel/Localizable.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+
+namespace LuaSTGEditorSharpV2.ViewModel
+{
+    public abstract record class Localizable<T>(
+        T? Neutral = null,
+        Dictionary<string, T>? Localized = null)
+        where T : class
+    {
+        [JsonIgnore] public abstract T Default { get; }
+
+        public T GetLocalized(T? @default = null)
+        {
+            @default ??= Default;
+            var str = Neutral ?? Default;
+            if (Localized == null)
+            {
+                return str;
+            }
+            else
+            {
+                return Localized?.GetValueOrDefault(CultureInfo.CurrentUICulture.TwoLetterISOLanguageName
+                    , str) ?? @default;
+            }
+        }
+    }
+
+    public record class LocalizableString : Localizable<string>
+    {
+        public override string Default => string.Empty;
+    }
+
+    public record class LocalizableArray<T> : Localizable<T[]>
+    {
+        public override T[] Default => Array.Empty<T>();
+    }
+}
