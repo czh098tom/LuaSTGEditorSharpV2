5fa7e131024285552abdcbf5099fee948712ef9a
czh098tom <czh098tom@163.com>
2024-02-06 22:49:17 +0800

    try to add drag

--- src/LuaSTGEditorSharpV2.WPF/MultiSelectDragableTreeView.cs
+++ src/LuaSTGEditorSharpV2.WPF/MultiSelectDragableTreeView.cs
@@ -1,13 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Intrinsics.Arm;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
+
+using GongSolutions.Wpf.DragDrop;
+using GongSolutions.Wpf.DragDrop.Utilities;
+using DragDrop = GongSolutions.Wpf.DragDrop.DragDrop;
 
 namespace LuaSTGEditorSharpV2.WPF
 {
     public class MultiSelectDragableTreeView : MultiSelectTreeView
     {
+        public MultiSelectDragableTreeView() : base()
+        {
+            DragDrop.SetIsDragSource(this, true);
+            DragDrop.SetIsDropTarget(this, true);
+            DragDrop.SetUseDefaultDragAdorner(this, true);
+            Style s = new(typeof(MultiSelectTreeViewItem));
+            s.Setters.Add(new Setter(DragDrop.IsDragSourceProperty, true));
+            s.Setters.Add(new Setter(DragDrop.IsDropTargetProperty, true));
+            s.Setters.Add(new Setter(PaddingProperty, new Thickness(0)));
+            s.Setters.Add(new Setter(MultiSelectTreeViewItem.IsExpandedProperty, true));
+            Resources.Add(typeof(MultiSelectTreeViewItem), s);
+        }
     }
 }
