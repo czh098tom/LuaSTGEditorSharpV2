099b998e6338315a1d9f1f5d936661cb02f2fe4b
czh098tom <czh098tom@163.com>
2024-02-09 00:56:12 +0800

    add IDropTarget implementation

--- src/LuaSTGEditorSharpV2.View/DocumentTreeView.xaml.cs
+++ src/LuaSTGEditorSharpV2.View/DocumentTreeView.xaml.cs
@@ -13,16 +13,39 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using GongSolutions.Wpf.DragDrop;
+
 namespace LuaSTGEditorSharpV2.View
 {
     /// <summary>
     /// DocumentTreeView.xaml 的交互逻辑
     /// </summary>
-    public partial class DocumentTreeView : TreeView
+    public partial class DocumentTreeView : TreeView, IDropTarget
     {
+        public class DropEventArgs(IDropInfo dropInfo) : EventArgs
+        {
+            public IDropInfo DropInfo { get; set; } = dropInfo;
+        }
+
+        public event EventHandler<DropEventArgs>? Dropping;
+        public event EventHandler<DropEventArgs>? DraggingOver;
+
         public DocumentTreeView()
         {
             InitializeComponent();
+            GongSolutions.Wpf.DragDrop.DragDrop.SetDropHandler(this, this);
+        }
+
+        void IDropTarget.DragOver(IDropInfo dropInfo)
+        {
+            DraggingOver?.Invoke(this, new DropEventArgs(dropInfo));
+            dropInfo.DropTargetAdorner = DropTargetAdorners.Highlight;
+            dropInfo.Effects = DragDropEffects.Move;
+        }
+
+        void IDropTarget.Drop(IDropInfo dropInfo)
+        {
+            Dropping?.Invoke(this, new DropEventArgs(dropInfo));
         }
     }
 }
