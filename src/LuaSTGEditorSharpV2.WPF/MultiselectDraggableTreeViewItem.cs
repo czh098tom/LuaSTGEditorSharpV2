8b114d16a68d022b707c6b28d907c4bb95e4d86f
czh098tom <czh098tom@163.com>
2024-02-14 15:39:57 +0800

    add interfaces

--- src/LuaSTGEditorSharpV2.WPF/MultiselectDraggableTreeViewItem.cs
+++ src/LuaSTGEditorSharpV2.WPF/MultiselectDraggableTreeViewItem.cs
@@ -1,33 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.Intrinsics.Arm;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
 
 using GongSolutions.Wpf.DragDrop;
-using GongSolutions.Wpf.DragDrop.Utilities;
 
 using DragDrop = GongSolutions.Wpf.DragDrop.DragDrop;
 
 namespace LuaSTGEditorSharpV2.WPF
 {
-    public class MultiSelectDragableTreeView : MultiSelectTreeView, IDragSource, IDropTarget
+    public class MultiselectDraggableTreeViewItem : MultiSelectTreeViewItem, IDragSource, IDropTarget
     {
-        public MultiSelectDragableTreeView() : base()
+        public MultiselectDraggableTreeViewItem() : base()
         {
-            DragDrop.SetIsDragSource(this, true);
-            DragDrop.SetIsDropTarget(this, true);
             DragDrop.SetDropHandler(this, this);
             DragDrop.SetDragHandler(this, this);
-            Style s = new(typeof(MultiSelectTreeViewItem));
-            s.Setters.Add(new Setter(DragDrop.IsDragSourceProperty, true));
-            s.Setters.Add(new Setter(DragDrop.IsDropTargetProperty, true));
-            s.Setters.Add(new Setter(PaddingProperty, new Thickness(0)));
-            s.Setters.Add(new Setter(MultiSelectTreeViewItem.IsExpandedProperty, true));
-            Resources.Add(typeof(MultiSelectTreeViewItem), s);
         }
 
         public bool CanStartDrag(IDragInfo dragInfo)
