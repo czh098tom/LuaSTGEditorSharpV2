efa3d7fd07210b258a17493c8117f4e8136b5abb
czh098tom <czh098tom@163.com>
2024-02-16 14:03:17 +0800

    modified toolbox appearance

---
+++ src/LuaSTGEditorSharpV2.Toolbox/HasCommandSelector.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LuaSTGEditorSharpV2.Toolbox.ViewModel;
+using LuaSTGEditorSharpV2.WPF;
+
+namespace LuaSTGEditorSharpV2.Toolbox
+{
+    public class HasCommandSelector : ResourceDictKeySelector<ToolboxItemViewModel>
+    {
+        public override string CreateKey(ToolboxItemViewModel vm)
+        {
+            return vm.HasCommand.ToString();
+        }
+
+        public override bool HasKeyFromSource(ToolboxItemViewModel vm)
+        {
+            return true;
+        }
+    }
+}
