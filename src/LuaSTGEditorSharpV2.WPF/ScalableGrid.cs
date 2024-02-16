dd53ab91cc0dae0fa7b1f4bc886d633f39524bda
czh098tom <czh098tom@163.com>
2024-02-16 16:56:03 +0800

    add scalablegrid to support zooming window elements by pressing ctrl+/-

---
+++ src/LuaSTGEditorSharpV2.WPF/ScalableGrid.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LuaSTGEditorSharpV2.WPF
+{
+    public partial class ScalableGrid : Grid, IScalable
+    {
+        public static readonly DependencyProperty ScaleProperty;
+
+        static ScalableGrid()
+        {
+            ScaleProperty = ScalableHelper.RegisterScalePropertyFor(nameof(Scale),
+                typeof(ScalableGrid));
+        }
+
+        public double Scale
+        {
+            get => (double)GetValue(ScaleProperty);
+            set => SetValue(ScaleProperty, value);
+        }
+
+        private readonly ScaleTransform _scaleTransform;
+
+        public ScalableGrid() : base()
+        {
+            _scaleTransform = new ScaleTransform(Scale, Scale, 0, 0);
+            LayoutTransform = _scaleTransform;
+
+            CommandBindings.AddRange(new CommandBinding[]
+            {
+                new(ScalableHelper.ZoomIn, ZoomIn_Executed),
+                new(ScalableHelper.ZoomOut, ZoomOut_Executed),
+            });
+        }
+
+        private void ZoomIn_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            Scale += 0.1;
+        }
+
+        private void ZoomOut_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            Scale -= 0.1;
+        }
+
+        public void SetScale(double scale)
+        {
+            _scaleTransform.ScaleX = scale;
+            _scaleTransform.ScaleY = scale;
+        }
+    }
+}
---
+++ src/LuaSTGEditorSharpV2.WPF/ScalableHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace LuaSTGEditorSharpV2.WPF
+{
+    public static class ScalableHelper
+    {
+        public static readonly RoutedUICommand ZoomIn;
+        public static readonly RoutedUICommand ZoomOut;
+
+        static ScalableHelper()
+        {
+            ZoomIn = new RoutedUICommand("Zoom In", "ZoomIn", typeof(ScalableHelper),
+                [new KeyGesture(Key.OemPlus, ModifierKeys.Control)]);
+            ZoomOut = new RoutedUICommand("Zoom Out", "ZoomOut", typeof(ScalableHelper),
+                [new KeyGesture(Key.OemMinus, ModifierKeys.Control)]);
+        }
+
+        public static DependencyProperty RegisterScalePropertyFor(string name, Type t)
+        {
+            return DependencyProperty.Register(
+                name,
+                typeof(double),
+                t,
+                new PropertyMetadata(1.0, OnScalePropertyChanged, CoerceScaleProperty));
+        }
+
+        private static void OnScalePropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is not IScalable sg) return;
+            var scaleVal = (double)e.NewValue;
+            sg.SetScale(scaleVal);
+        }
+
+        private static object CoerceScaleProperty(DependencyObject d, object value)
+        {
+            var val = (double)value;
+            if (double.IsNaN(val)) return 1.0;
+            val = Math.Clamp(val, 0.1, 100);
+            return val;
+        }
+    }
+}
