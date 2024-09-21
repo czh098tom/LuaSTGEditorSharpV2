b5bc46e6fbb1c99d40b308b2628b95d036cb44b3
czh098tom <czh098tom@163.com>
2024-09-21 18:09:52 +0800

    add serialization short naming service

---
+++ src/LuaSTGEditorSharpV2.Core/JsonTypeShortNameAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuaSTGEditorSharpV2.Core
+{
+    [AttributeUsage(AttributeTargets.Class, Inherited = false)]
+    public class JsonTypeShortNameAttribute(Type sourceType, string name) : Attribute
+    {
+        public Type SourceType { get; } = sourceType;
+        public string Name { get; } = name;
+    }
+}
