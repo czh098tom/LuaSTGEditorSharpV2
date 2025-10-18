70678dcb21e8224ce718c5134b497fd5babde477
Airo <bigod857861926@163.com>
2025-10-18 13:24:56 +0800

    add mustache code generation
    (cherry picked from commit 5775d57687684fcd7e2e8240eb9409b368ec73db)

---
+++ src/LuaSTGEditorSharpV2.Core.CodeGenerator/Configurable/Mustache/ContextCapture.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuaSTGEditorSharpV2.Core.CodeGenerator.Configurable.Mustache
+{
+	[Serializable]
+	public record class ContextCapture
+	{
+		[JsonProperty] public string TypeUID { get; private set; }
+		[JsonProperty] public Dictionary<string, CaptureWithMacroOption> Property { get; private set; }
+
+		public ContextCapture(string typeUID, Dictionary<string, CaptureWithMacroOption> property)
+		{
+			TypeUID = typeUID;
+			Property = property;
+		}
+	}
+}
