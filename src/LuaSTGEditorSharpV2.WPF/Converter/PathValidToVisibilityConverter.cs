40c78de58c5f41ae2687ede0f41cb9024b85dd80
czh098tom <czh098tom@163.com>
2025-10-18 18:25:50 +0800

    add choose image property view

---
+++ src/LuaSTGEditorSharpV2.WPF/Converter/PathValidToVisibilityConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace LuaSTGEditorSharpV2.WPF.Converter
+{
+    [ValueConversion(typeof(string), typeof(Visibility))]
+    public class PathValidToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var inValidVisibility = parameter == null ? Visibility.Collapsed : (Visibility)parameter;
+            var isVisible = Path.Exists(value?.ToString());
+            return isVisible ? Visibility.Visible : inValidVisibility;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
