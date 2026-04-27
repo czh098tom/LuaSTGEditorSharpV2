a6fc1197ad50b8ade1aa63f7c6ed91aaf35227b5
Airo <bigod857861926@163.com>
2026-04-27 16:10:37 +0800

    [main]-add vector2 propview editor
    (cherry picked from commit edfdc05872b74656d31cb1bd82faa32f59f5e46d)

---
+++ src/LuaSTGEditorSharpV2.PropertyView/Specialized/Vector/Vector2PropertyItemTerm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using LuaSTGEditorSharpV2.Core;
+using LuaSTGEditorSharpV2.Core.Editor;
+using LuaSTGEditorSharpV2.PropertyView.Configurable;
+using LuaSTGEditorSharpV2.PropertyView.ViewModel;
+
+namespace LuaSTGEditorSharpV2.PropertyView.Specialized.Vector;
+
+[JsonUseShortNaming]
+[JsonTypeShortName(typeof(IPropertyItemTerm), "Vector2")]
+public class Vector2PropertyItemTerm(IServiceProvider serviceProvider) : PropertyItemTerm(serviceProvider)
+{
+    public override PropertyItemViewModelBase GetViewModel(EditorNode nodeData, PropertyViewContext context)
+    {
+        return GetViewModelImpl<Vector2PropertyItemViewModel>(nodeData, context);
+    }
+}
