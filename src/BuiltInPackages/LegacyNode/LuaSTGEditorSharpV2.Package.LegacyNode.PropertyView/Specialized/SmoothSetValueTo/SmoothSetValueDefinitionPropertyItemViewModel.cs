76f5bda7d37dda9359924dec7a70142fc61c0172
Airo <bigod857861926@163.com>
2026-08-30 15:24:48 +0800

    [pkg]-migrate SmoothSetValueTo editor to BoundProperty

--- src/BuiltInPackages/LegacyNode/LuaSTGEditorSharpV2.Package.LegacyNode.PropertyView/Specialized/SmoothSetValueTo/SmoothSetValueDefinitionPropertyItemViewModel.cs
+++ src/BuiltInPackages/LegacyNode/LuaSTGEditorSharpV2.Package.LegacyNode.PropertyView/Specialized/SmoothSetValueTo/SmoothSetValueDefinitionPropertyItemViewModel.cs
@@ -8,48 +8,48 @@ namespace LuaSTGEditorSharpV2.Package.LegacyNode.PropertyView.Specialized.Smooth
 public class SmoothSetValueDefinitionPropertyItemViewModel
     : BoundPropertyItemViewModelBase<SmoothSetValuePropertyViewItemListTerm.ItemTerm>
 {
-    private readonly BoundProperty _variableNameProperty = new();
-    private readonly BoundProperty _targetValueProperty = new();
-    private readonly BoundProperty _interpolationModeProperty = new();
-    private readonly BoundProperty _modificationModeProperty = new();
+    public BoundProperty VariableNameProperty { get; } = new();
+    public BoundProperty TargetValueProperty { get; } = new();
+    public BoundProperty InterpolationModeProperty { get; } = new();
+    public BoundProperty ModificationModeProperty { get; } = new();
 
     public string VariableName
     {
-        get => _variableNameProperty.Value;
-        set => _variableNameProperty.Value = value;
+        get => VariableNameProperty.Value;
+        set => VariableNameProperty.Value = value;
     }
 
     public string TargetValue
     {
-        get => _targetValueProperty.Value;
-        set => _targetValueProperty.Value = value;
+        get => TargetValueProperty.Value;
+        set => TargetValueProperty.Value = value;
     }
 
     public string InterpolationMode
     {
-        get => _interpolationModeProperty.Value;
-        set => _interpolationModeProperty.Value = value;
+        get => InterpolationModeProperty.Value;
+        set => InterpolationModeProperty.Value = value;
     }
 
     public string ModificationMode
     {
-        get => _modificationModeProperty.Value;
-        set => _modificationModeProperty.Value = value;
+        get => ModificationModeProperty.Value;
+        set => ModificationModeProperty.Value = value;
     }
 
     protected override void ConfigureViewModel(SmoothSetValuePropertyViewItemListTerm.ItemTerm term)
     {
-        ForwardValueChanges(_variableNameProperty, nameof(VariableName));
-        ForwardValueChanges(_targetValueProperty, nameof(TargetValue));
-        ForwardValueChanges(_interpolationModeProperty, nameof(InterpolationMode));
-        ForwardValueChanges(_modificationModeProperty, nameof(ModificationMode));
+        ForwardValueChanges(VariableNameProperty, nameof(VariableName));
+        ForwardValueChanges(TargetValueProperty, nameof(TargetValue));
+        ForwardValueChanges(InterpolationModeProperty, nameof(InterpolationMode));
+        ForwardValueChanges(ModificationModeProperty, nameof(ModificationMode));
     }
 
     protected override void ConfigureBinding(SmoothSetValuePropertyViewItemListTerm.ItemTerm term)
 	{
-		Bind(term.VariableName).ToOne(_variableNameProperty);
-		Bind(term.TargetValue).ToOne(_targetValueProperty);
-		Bind(term.InterpolationMode).ToOne(_interpolationModeProperty);
-		Bind(term.ModificationMode).ToOne(_modificationModeProperty);
+		Bind(term.VariableName).ToOne(VariableNameProperty);
+		Bind(term.TargetValue).ToOne(TargetValueProperty);
+		Bind(term.InterpolationMode).ToOne(InterpolationModeProperty);
+		Bind(term.ModificationMode).ToOne(ModificationModeProperty);
 	}
 }
